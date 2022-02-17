5943e8f712823dff47b8ce8b0e5e86e48ae04be5
Don Carlos Zendejas <czende@hotmail.com>
2022-02-17 08:44:28 -0600

    Update Starter.tsx
    add cubes component

--- src/worlds/Starter.tsx
+++ src/worlds/Starter.tsx
@@ -1,11 +1,13 @@
 import { Spinning, Floating, StandardEnvironment } from "spacesvr";
 import TransparentFloor from "../ideas/TransparentFloor";
 import CloudySky from "../ideas/CloudySky";
+import Cubes from "../ideas/Cubes";
 
 export default function Starter() {
   return (
     <StandardEnvironment>
       <ambientLight />
+      <Cubes />      
       <group position={[0, 0, -4]}>
         <Floating>
           <Spinning xSpeed={0.2} ySpeed={0.4} zSpeed={0.3}>
