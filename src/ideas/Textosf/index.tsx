18491ea7459735117896d8fd5a13b42c0e8b27a2
Don Carlos Zendejas <czende@hotmail.com>
2022-02-17 09:17:30 -0600

    Update index.tsx

--- src/ideas/Textosf/index.tsx
+++ src/ideas/Textosf/index.tsx
@@ -6,44 +6,20 @@ const COUNT = 150;
 const RANGE_XZ = 50;
 const RANGE_Y = 30;
 
-type Cubo2 = {
-  position: [number, number, number];
-  size: number;
-  color: Color;
-  speed: number;
-  texto: string;
-};
+
 
 type Libro = {
   indice: number;
   titulo: string;
 };
 
+//type MyProps = {
+  // using `interface` is also ok
+//  stocks: string;
+//};
+//const Textosf = (props:MyProps) => {
 const Textosf = (props) => {
-  const cubos2: Cubo2[] = useMemo(() => {
-    const arrStocks: Libro[] = [];
-    props.stocks.forEach((element: any) => {
-      console.log("elemento es");
-      console.log(element);
-      arrStocks.push({ indice: 1, titulo: element.name });
-    });
 
-    const arr: Cubo2[] = [];
-    for (let i = 0; i < COUNT; i++) {
-      arr.push({
-        position: [
-          Math.random() * RANGE_XZ * 2 - RANGE_XZ,
-          Math.random() * RANGE_Y,
-          Math.random() * RANGE_XZ * 2 - RANGE_XZ
-        ],
-        size: 0.5 + Math.random() * 2.5,
-        color: new Color().setHSL(Math.random(), Math.random(), Math.random()),
-        speed: Math.random() + 0.2
-        //        texto: arrStocks && arrStocks[0].titulo,
-      });
-    }
-    return arr;
-  }, []);
 
   const arrStocks: Libro[] = useMemo(() => {
     const arr2: Libro[] = [];
@@ -74,22 +50,7 @@ const Textosf = (props) => {
         ))}
     </group>
 
-    /*<group>
-{cubos2.map((cubo2) => (
-  // @ts-ignore
-  <Floating height={cubo2.size * 1.5} speed={cubo2.speed}>
-    <Text
-        text={props.parametrito.titulo}
-     // text="Aqui esta los que buscas"
-      vAlign="center" // vertical align relative to the y component
-      hAlign="center" // horizontal align relative to the x component
-      size={1} // scale
-      color="#000000" // color
-    ></Text>
-  </Floating>
-))}
-</group>
-*/
+
   );
 };
 
